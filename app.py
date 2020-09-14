33fd6b8b726f11de06baf33c00f3a7830152692c
Neto Costa <costa.neto@unesp.br>
2020-09-14 17:17:45 -0300

    Form Handling
    Implementações do Projeto durante o capítulo Form Handling.

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from  flask import Flask,render_template,abort
+from forms import LoginForm,SignUpForm
+from flask import session, redirect, url_for
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'dfewfew123213rwdsgert34tgfd1234trgf'
 
 """Information regarding the Pets in the System."""
 pets = [
@@ -10,9 +14,43 @@ pets = [
             {"id": 4, "name": "Mr. Furrkins", "age": "5 years", "bio": "Probably napping."}, 
         ]
 
+"""Information regarding the Users in the System."""
+users = [
+            {"id": 1, "full_name": "Pet Rescue Team", "email": "team@pawsrescue.co", "password": "adminpass"},
+        ]
+
+
 @app.route("/")
 def home():
     return render_template("home.html", pets=pets)
+
+@app.route("/signup", methods=["POST", "GET"])
+def signup():
+    """View function for Sign Up.""" 
+    form = SignUpForm()
+    if form.validate_on_submit():
+        new_user = {"id": len(users)+1, "full_name": form.full_name.data, "email": form.email.data, "password": form.password.data}
+        users.append(new_user)
+        return render_template("signup.html", message = "Successfully signed up")
+    return render_template("signup.html", form = form)
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = next((user for user in users if user["email"] == form.email.data and user["password"] == form.password.data), None)
+        if user is None:
+            return render_template("login.html", form = form, message = "Wrong Credentials. Please Try Again.")
+        else:
+            session['user'] = user
+            return render_template("login.html", message = "Successfully Logged In!")
+    return render_template("login.html", form = form)
+
+@app.route("/logout")
+def logout():
+    if 'user' in session:
+        session.pop('user')
+    return redirect(url_for('home', _scheme='https', _external=True))
 
 @app.route("/about")
 def about():
